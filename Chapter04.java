cee6fbb7303587beabde207689825d1dd812ef32
615141048312 <hanada.yui@mbr.nifty.com>
2020-08-24 17:08:38 +0900

    finish chapter04

---
+++ Chapter04.java
@@ -0,0 +1,13 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String name = "Yui Hanada";
+		System.out.println(name);
+		
+		name = "Ghillie Bat";
+		System.out.println(name);
+		
+		final int LUCKY_NUMBER = 8312;
+		System.out.println(LUCKY_NUMBER);
+	}
+}
